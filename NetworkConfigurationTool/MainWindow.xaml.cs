b6b1233e0751e16b214630ff8dd341b34a43a8ec
Asad Zarnoosheh <Asad@DadekavanCo.com>
2024-02-12 10:30:46 +0100

    Add project files.

---
+++ NetworkConfigurationTool/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using NetworkConfigurationTool.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace NetworkConfigurationTool
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public ObservableCollection<NetworkAdapterInfo> NetworkAdapters { get; set; }
+        private DispatcherTimer timer;
+        public MainWindow()
+		{
+			InitializeComponent();
+			NetworkAdapters = new ObservableCollection<NetworkAdapterInfo>();
+			DataContext = this;
+			LoadNetworkAdapters();
+
+
+            // Set up the timer to refresh every 5 seconds
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            NetworkAdapters.Clear();
+            LoadNetworkAdapters();
+        }
+
+        private void LoadNetworkAdapters()
+		{
+			var adapters = NetworkInterface.GetAllNetworkInterfaces();
+			foreach (var adapter in adapters)
+			{
+				var iPAddressInformation = adapter.GetIPProperties().UnicastAddresses.
+					FirstOrDefault(p => p.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+
+				NetworkAdapters.Add(new NetworkAdapterInfo
+				{
+					Name = adapter.Name,
+					Status = adapter.OperationalStatus.ToString(),
+					IPAddress = adapter.OperationalStatus == OperationalStatus.Up ? iPAddressInformation?.Address.ToString() : "",
+					SubNetMask = adapter.OperationalStatus == OperationalStatus.Up ? iPAddressInformation?.IPv4Mask.ToString() : ""
+				});
+			}
+		}
+	}
+}
---
+++ NetworkConfigurationTool/Models/NetworkAdapterInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkConfigurationTool.Models
+{
+	public class NetworkAdapterInfo : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private string _name;
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; NotifyPropertyChanged("Name"); }
+		}
+
+		private string _status;
+		public string Status
+		{
+			get { return _status; }
+			set { _status = value; NotifyPropertyChanged("Status"); }
+		}
+
+		private string _ipAddress;
+		public string IPAddress
+		{
+			get { return _ipAddress; }
+			set { _ipAddress = value; NotifyPropertyChanged("IPAddress"); }
+		}
+
+
+		private string _subNetMask;
+		public string SubNetMask
+		{
+			get { return _subNetMask; }
+			set { _subNetMask = value; NotifyPropertyChanged("SubNetMask"); }
+		}
+		private void NotifyPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
